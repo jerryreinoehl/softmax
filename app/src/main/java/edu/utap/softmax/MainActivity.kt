da1c9dd4e805ebc365b6c988e9976df93f042bd8
Jerry Reinoehl <jerryreinoehl@gmail.com>
2022-11-19 13:38:37 -0600

    Add initial MainActivity

--- app/src/main/java/edu/utap/softmax/MainActivity.kt
+++ app/src/main/java/edu/utap/softmax/MainActivity.kt
@@ -1,11 +1,16 @@
 package edu.utap.softmax
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.fetchModels()
     }
 }
