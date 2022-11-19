c3b26ff83de90407dfea0dc52f884ecc12c512e6
Jerry Reinoehl <jerryreinoehl@gmail.com>
2022-11-19 16:27:19 -0600

    Create initial ModelActivity and layout

---
+++ app/src/main/java/edu/utap/softmax/ModelActivity.kt
@@ -0,0 +1,10 @@
+package edu.utap.softmax
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ModelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
